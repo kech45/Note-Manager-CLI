03341abc18c0f93b3fc6125756fc02352d8a5b05
Kerem Halil <keremsunayhalil@gmail.com>
2026-03-29 17:26:29 +0300

    Add storage layer with embedded SQLite

---
+++ notes/storage.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+DB_PATH = "data/note_manager.db"
+
+def initialize_db():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS notes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            content TEXT,
+            created_at VARCHAR(255) DEFAULT (datetime('now'))
+        )
+    """)
+    conn.commit()
+    conn.close()
+    
+def add_note(title: str, content: str):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO notes(title, content) VALUES (?, ?)", (title,content))
+    conn.commit()
+    conn.close()
+    
+def get_all_notes():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM notes")
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
+def get_note_by_given_id(id : int):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM notes WHERE id = ?", (id,))
+    row = cursor.fetchone()
+    conn.close()
+    return row
+
+def delete_note_by_given_id(id : int):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM notes WHERE id = ?", (id,))
+    conn.commit()
+    conn.close()
+    
+
